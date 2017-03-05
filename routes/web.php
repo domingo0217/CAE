73fdf9ccc12af8d293ae99bc9d6a996301f2bcd8
domingo0217 <domingo1702@gmail.com>
2017-03-05 17:33:45 -0400

    Materialize y rutas
    configurando materialize para trabajar en laravel con composer y
    especificando rutas de index y main. Trabajando con blade templates.

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,10 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/', function (){
+    return view('index');
+});
+
+Route::get('main', function (){
+    return view('main');
 });
